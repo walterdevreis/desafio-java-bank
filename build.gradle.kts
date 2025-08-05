18e6f8f0d21da54c18885f5d075cddaa3b93db2b
walter dev reis <walter-reis@outlook.com>
2025-08-05 12:49:39 -0300

    Criação e configuração do projeto desafio java bank adicionando as dependencias do lombok no arquivo build.gradle para diminuir a verbosidade do código e melhorar a legibilidade do mesmo

---
+++ build.gradle.kts
@@ -0,0 +1,29 @@
+plugins {
+    id("java")
+}
+
+configurations {
+    compileOnly {
+        extendsFrom(configurations.annotationProcessor.get())
+    }
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+
+    compileOnly ("org.projectlombok:lombok:1.18.38")
+    annotationProcessor ("org.projectlombok:lombok:1.18.38")
+
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
